5e6816dd3cdfd3a3f3489bcc8d6c791b516dcffe
aditya-0212 <adityachoubisa02@gmail.com>
2024-05-15 10:27:23 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by APPLE on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
